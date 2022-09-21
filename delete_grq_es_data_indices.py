f9b80c36344aece66b1d3d0a198785b2bc6ed7b1
Philip Yoon <philip.j.yoon@jpl.nasa.gov>
2022-09-21 20:38:35 +0000

    Two new scripts to automate reseting GRQ ES

---
+++ delete_grq_es_data_indices.py
@@ -0,0 +1,12 @@
+from hysds.es_util import get_grq_es
+
+grq_es = get_grq_es()
+
+print("Deleting data indices from", grq_es.es_url)
+_data_indices = ['grq_v2.0_l2_hls_l30', 'grq_v2.0_l2_hls_s30', 'grq_1_opera_state_config', 'grq_v1.1.5_triaged_job',
+        'grq_v0.1_l3_dswx_hls', 'grq_1_l2_hls_s30-state-config', 'grq_1_l2_hls_l30-state-config']
+
+for i in _data_indices:
+    print("Deleting index", i)
+    print(grq_es.es.indices.delete(index=i, ignore=[404]))
+
