0c65c84fda57323c9936e485f2f74af0d8378310
philipjyoon <104090313+philipjyoon@users.noreply.github.com>
2023-05-01 14:06:12 -0700

    Update reset_grq_es.sh
    @maseca contribution

--- reset_grq_es.sh
+++ reset_grq_es.sh
@@ -2,5 +2,9 @@ python /export/home/hysdsops/mozart/ops/opera-pcm/data_subscriber/hls/delete_hls
 python /export/home/hysdsops/mozart/ops/opera-pcm/data_subscriber/hls/create_hls_catalog.py
 python /export/home/hysdsops/mozart/ops/opera-pcm/data_subscriber/hls_spatial/delete_hls_spatial_catalog.py
 python /export/home/hysdsops/mozart/ops/opera-pcm/data_subscriber/hls_spatial/create_hls_spatial_catalog.py
+python /export/home/hysdsops/mozart/ops/opera-pcm/data_subscriber/slc/delete_slc_catalog.py
+python /export/home/hysdsops/mozart/ops/opera-pcm/data_subscriber/slc/create_slc_catalog.py
+python /export/home/hysdsops/mozart/ops/opera-pcm/data_subscriber/slc_spatial/delete_slc_spatial_catalog.py
+python /export/home/hysdsops/mozart/ops/opera-pcm/data_subscriber/slc_spatial/create_slc_spatial_catalog.py
 python /export/home/hysdsops/mozart/ops/opera-pcm/job_accountability/create_job_accountability_catalog.py --delete_old_catalog
 python delete_grq_es_data_indices.py
