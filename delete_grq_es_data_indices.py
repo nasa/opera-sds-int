fc1cd0549bfa7cac1cc6fb43b95525cf9915c36f
philipjyoon <104090313+philipjyoon@users.noreply.github.com>
2023-05-01 14:07:36 -0700

    Update delete_grq_es_data_indices.py

--- delete_grq_es_data_indices.py
+++ delete_grq_es_data_indices.py
@@ -4,7 +4,8 @@ grq_es = get_grq_es()
 
 print("Deleting data indices from", grq_es.es_url)
 _data_indices = ['grq_v2.0_l2_hls_l30', 'grq_v2.0_l2_hls_s30', 'grq_1_opera_state_config', 'grq_v1.1.5_triaged_job',
-        'grq_v0.1_l3_dswx_hls','grq_v1.0_l3_dswx_hls', 'grq_1_l2_hls_s30-state-config', 'grq_1_l2_hls_l30-state-config']
+        'grq_v0.1_l3_dswx_hls','grq_v1.0_l3_dswx_hls', 'grq_1_l2_hls_s30-state-config', 'grq_1_l2_hls_l30-state-config',
+        'grq_v0.0_l2_rtc_s1', 'grq_v0.0_l2_cslc_s1']
 
 for i in _data_indices:
     print("Deleting index", i)
