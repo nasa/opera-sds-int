cb5240ba1dbd1bd959ea5bbcd82766dfeb34b706
Philip Yoon <philip.j.yoon@jpl.nasa.gov>
2023-06-16 10:45:41 -0700

    Working towards S3 upload feature

--- GEE_upload_scripts/run-Convert-CSLCtoCOG.py
+++ GEE_upload_scripts/run-Convert-CSLCtoCOG.py
@@ -9,17 +9,13 @@ from botocore import UNSIGNED
 from botocore.client import Config
 import multiprocessing as mp
 
-
-
-
-
 def upload_blob(bucket_name, source_file_name, destination_blob_name):
     storage_client = storage.Client()
     bucket = storage_client.get_bucket(bucket_name,timeout=None)
     blob = bucket.blob(destination_blob_name)
     blob.upload_from_filename(source_file_name,timeout=None)
 
-def processCSLC(s3key,gcskey):
+def processCSLC(bucket, s3key,gcskey):
     filename = s3key.split('/')[-1]
     if os.path.exists(f'./temp_{filename}/'):
         shutil.rmtree(f'./temp_{filename}/')
@@ -28,7 +24,6 @@ def processCSLC(s3key,gcskey):
     #Download CSLC file
     #print('Downloading CSLC File')
     s3 = boto3.client('s3', config=Config(signature_version=UNSIGNED))
-    bucket = 'opera-pst-rs-pop1'
     filename = s3key.split('/')[-1]
     filepath = f'./temp_{filename}/'+filename
     s3.download_file(bucket, s3key, filepath)
@@ -55,48 +50,65 @@ def processCSLC(s3key,gcskey):
     upload_blob(gcsbucket,outfile,gcskey)
     shutil.rmtree(f'./temp_{filename}/')
 
-def run_rtc_transfer(keydict):
+def run_rtc_transfer(bucket, keydict):
     try:
         start_time = time.time()
         s3key = keydict['s3key']
         gcskey = keydict['gcsKey']
-        processCSLC(s3key,gcskey)
+        processCSLC(bucket, s3key, gcskey)
         print(f'[{time.time() - start_time}] Uploaded to: {gcskey}')
     except Exception as e: 
         print(f'Failed on {s3key}, error: {e}')
 
-if __name__ == '__main__':
-    os.environ["GCLOUD_PROJECT"] = "opera-one"
-    s3_prefix = 'products/int_fwd_r2/2023-05-04_globalrun_2021-04-11_to_2021-04-22/CSLC_S1/'
-    gcs_prefix = 'products/2023-05-04_globalrun_2021-04-11_to_2021-04-22_VV/'
-    s3 = boto3.client('s3', config=Config(signature_version=UNSIGNED))
+def h5_to_key(path):
+    return path.split('/')[-2]+'.h5'
+
+def tif_to_key(path):
+    return path.split('.tif')[0][:-2]+'.tif'
+
+def find_s3_keys(s3, s3_prefix, path_to_key):
     paginator = s3.get_paginator('list_objects_v2')
-    pages = paginator.paginate(Bucket='opera-pst-rs-pop1', Prefix=s3_prefix, Delimiter='/')
+    pages = paginator.paginate(Bucket=s3_bucket, Prefix=s3_prefix, Delimiter='/')
     keyList = []
     for page in pages:
         #pprint(page)
         for obj in page['CommonPrefixes']:
             path = obj.get('Prefix')
-            fname = path.split('/')[-2]+'.h5'
+            fname = path_to_key(path)
             keyList.append(path+fname)
-    print(f'{len(keyList)} s3 keys found')
 
-    storage_client = storage.Client()
+    return keyList
+
+if __name__ == '__main__':
+    bucket = 'opera-pst-rs-pop1'
+    os.environ["GCLOUD_PROJECT"] = "opera-one"
+    s3_prefix = 'products/int_fwd_r2/2023-05-04_globalrun_2021-04-11_to_2021-04-22/CSLC_S1/'
+    dest_prefix = 'products/int_fwd_r2/2023-05-04_globalrun_2021-04-11_to_2021-04-22/CSLC_S1_Converted/'
+    #gcs_prefix = 'products/2023-05-04_globalrun_2021-04-11_to_2021-04-22_VV/'
+    s3 = boto3.client('s3', config=Config(signature_version=UNSIGNED))
+
+    keyList = find_s3_keys(s3, s3_prefix, h5_to_key)
+    print(f'{len(keyList)} source s3 keys found')
+
+    dest_keys = find_s3_keys(s3, dest_prefix, tif_to_key)
+    print(f'{len(dest_keys)} destination s3 keys found')
+
+    '''storage_client = storage.Client()
     blobs = storage_client.list_blobs("opera-bucket-cslc", prefix=gcs_prefix)
     gcsKeys = []
     for blob in blobs:
         gcsKeys.append(blob.name.split('.tif')[0][:-2]+'.tif')
-    print(f'{len(gcsKeys)} existing gcs keys found')
+    print(f'{len(gcsKeys)} existing gcs keys found')'''
 
     keyPairs = []
     for key in keyList:
         fname = key.split('/')[-1]
-        gcsKey = gcs_prefix+fname.split('.h5')[0]+'.tif'
-        if gcsKey not in gcsKeys:
-            keydict = {'s3key':key,'gcsKey':gcsKey}
+        dest_key = dest_prefix+fname.split('.h5')[0]+'.tif'
+        if dest_key not in dest_keys:
+            keydict = {'s3key':key, 'gcsKey':dest_key}
             keyPairs.append(keydict)
     print(f'{len(keyPairs)} key pairs identified')
 
     pool = mp.Pool(4)
-    pool.map(run_rtc_transfer,keyPairs)
+    pool.map(run_rtc_transfer, bucket, keyPairs)
     pool.close()
