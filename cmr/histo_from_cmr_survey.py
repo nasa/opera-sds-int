88273e73fe8e9a6f3b770afc30d1e800740751ce
Philip Yoon <philip.j.yoon@jpl.nasa.gov>
2023-08-02 11:19:43 -0700

    Minor improvement

--- cmr/histo_from_cmr_survey.py
+++ cmr/histo_from_cmr_survey.py
@@ -6,6 +6,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 temporal_time_limit = "2023-04-01T00:00:00.000Z"
+#temporal_time_limit = "2023-05-23T00:00:00.000Z"
 
 '''Expects CSV file w 4 columns, comma-separated:
 Granule ID (ignored)
@@ -15,11 +16,16 @@ Revision-Temporal Delta Hours'''
 
 _date_format_str = "%Y-%m-%dT%H:%M:%SZ"
 _date_format_str_cmr = _date_format_str[:-1] + ".%fZ"
+REVISION_COLUMN = 1
+TEMPORAL_COLUMN = 2
 
 temporal_time_limit = datetime.strptime(temporal_time_limit, _date_format_str_cmr)
 
 all_deltas = []
 row_count = 0
+
+#outfile = open('output.csv', 'w')
+#writer = csv.writer(outfile)
 
 with open(sys.argv[1], 'r') as input_file:
     csvreader = csv.reader(input_file)
@@ -28,9 +34,10 @@ with open(sys.argv[1], 'r') as input_file:
             continue
 
         row_count += 1
-        temporal_time = datetime.strptime(row[2], _date_format_str_cmr)
+        temporal_time = datetime.strptime(row[TEMPORAL_COLUMN], _date_format_str_cmr)
         if temporal_time >= temporal_time_limit:
             all_deltas.append(float(row[3]))
+            #writer.writerow(row)
 
     print(f"Out of total {row_count} entries {len(all_deltas)} entries had temporal time later than {temporal_time_limit}. Difference of {len(all_deltas)-row_count}")
 
@@ -40,7 +47,7 @@ with open(sys.argv[1], 'r') as input_file:
     #print(hist_e)
     #print(hist_v)
     plt.title(hist_title)
-    histo_file = sys.argv[1]+".svg"
+    histo_file = sys.argv[1]+".mod.svg"
     print("Saving histogram figure as " + histo_file)
     plt.savefig(histo_file, format="svg", dpi=1200)
     plt.show()
