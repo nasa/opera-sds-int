608f134b313d3554e35b151b8feb18b114f235d6
Philip Yoon <philip.j.yoon@jpl.nasa.gov>
2023-08-12 13:23:56 -0700

    Genericized GRQ delete so that we don't specify index names

--- delete_grq_es_data_indices.py
+++ delete_grq_es_data_indices.py
@@ -3,11 +3,8 @@ from hysds.es_util import get_grq_es
 grq_es = get_grq_es()
 
 print("Deleting data indices from", grq_es.es_url)
-_data_indices = ['grq_v2.0_l2_hls_l30', 'grq_v2.0_l2_hls_s30', 'grq_1_opera_state_config', 'grq_v1.1.5_triaged_job',
-        'grq_v0.1_l3_dswx_hls','grq_v1.0_l3_dswx_hls', 'grq_1_l2_hls_s30-state-config', 'grq_1_l2_hls_l30-state-config',
-        'grq_v0.0_l2_rtc_s1', 'grq_v0.0_l2_cslc_s1']
 
-for i in _data_indices:
-    print("Deleting index", i)
-    print(grq_es.es.indices.delete(index=i, ignore=[404]))
-
+for index in grq_es.es.indices.get_alias('*').keys():
+    if index[:4] == 'grq_' or index[:4] == 'hls_' or index[:4] == 'slc_' or index[:5] == 'jobs_':
+        print("Deleting index", index)
+        print(grq_es.es.indices.delete(index=index, ignore=[404]))
