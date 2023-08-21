3548b3fc44f0eac9f665038425760f0dbcfdd8a6
Scott Collins <collinss@jpl.nasa.gov>
2023-08-21 12:22:07 -0700

    Added product comparison scripts for R2 PGEs to r2_smoketest

---
+++ r2_smoketest/cslc_compare.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+output_dir=$1
+expected_dir=$2
+
+declare -a burst_ids=(  "t064_135518_iw1"
+                        "t064_135518_iw2"
+                        "t064_135518_iw3"
+                        "t064_135519_iw1"
+                        "t064_135519_iw2"
+                        "t064_135519_iw3"
+                        "t064_135520_iw1"
+                        "t064_135520_iw2"
+                        "t064_135520_iw3"
+                        "t064_135521_iw1"
+                        "t064_135521_iw2"
+                        "t064_135521_iw3"
+                        "t064_135522_iw1"
+                        "t064_135522_iw2"
+                        "t064_135522_iw3"
+                        "t064_135523_iw1"
+                        "t064_135523_iw2"
+                        "t064_135523_iw3"
+                        "t064_135524_iw1"
+                        "t064_135524_iw2"
+                        "t064_135524_iw3"
+                        "t064_135525_iw1"
+                        "t064_135525_iw2"
+                        "t064_135525_iw3"
+                        "t064_135526_iw1"
+                        "t064_135526_iw2"
+                        "t064_135526_iw3"
+                        "t064_135527_iw1")
+
+for burst_id in "${burst_ids[@]}"; do
+  echo "-------------------------------------"
+  echo "Comparing results for ${burst_id}"
+
+  burst_id_uppercase=${burst_id^^}
+  burst_id_replace_underscores=${burst_id_uppercase//_/-}
+  burst_id_pattern="OPERA_L2_CSLC-S1_${burst_id_replace_underscores}_*"
+
+  output_file=$(ls ${output_dir}/${burst_id_pattern}/*.h5)
+  expected_file=$(ls ${expected_dir}/expected_output_data/${burst_id_pattern}/*.h5)
+
+  python3 cslc_compare.py -r ${expected_file} -s ${output_file} -p CSLC
+done
