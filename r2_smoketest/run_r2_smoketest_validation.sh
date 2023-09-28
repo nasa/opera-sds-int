eb3e14aa421c7e428671ba952fd81a5754a9d53d
Philip Yoon <philip.j.yoon@jpl.nasa.gov>
2023-09-27 17:39:25 -0700

    Update to reflect gold file name convention change

--- r2_smoketest/run_r2_smoketest_validation.sh
+++ r2_smoketest/run_r2_smoketest_validation.sh
@@ -1,5 +1,5 @@
 # Arguments: CSLC version, RTC version, S3 R3 location, release
-# for example: 0.4.0, 0.4.1, opera-int-rs-fwd, 2.0.0-rc.10.0
+# for example: final_0.5.2 final_1.0.1 opera-int-rs-fwd 2.0.0
 cslc_v=$1
 rtc_v=$2
 s3_rs=$3
@@ -9,10 +9,10 @@ release=$4
 python3 ~/mozart/ops/opera-pcm/data_subscriber/daac_data_subscriber.py query -c SENTINEL-1A_SLC --release-version=${release} --processing-mode=reprocessing --job-queue=opera-job_worker-slc_data_download --chunk-size=1 --native-id=S1A_IW_SLC__1SDV_20220501T015035_20220501T015102_043011_0522A4_42CC*
 
 # Download and unzip gold files.
-wget https://artifactory-fn.jpl.nasa.gov:443/artifactory/general-stage/gov/nasa/jpl/opera/sds/pcm/testdata_R2.0.0/deployment_smoke_test_cslc_s1_calval_${cslc_v}.zip
-wget https://artifactory-fn.jpl.nasa.gov:443/artifactory/general-stage/gov/nasa/jpl/opera/sds/pcm/testdata_R2.0.0/deployment_smoke_test_rtc_s1_calval_${rtc_v}.zip
-unzip deployment_smoke_test_rtc_s1_calval_${rtc_v}.zip
-unzip deployment_smoke_test_cslc_s1_calval_${cslc_v}.zip
+wget https://artifactory-fn.jpl.nasa.gov:443/artifactory/general-stage/gov/nasa/jpl/opera/sds/pcm/testdata_R2.0.0/deployment_smoke_test_cslc_s1_${cslc_v}.zip
+wget https://artifactory-fn.jpl.nasa.gov:443/artifactory/general-stage/gov/nasa/jpl/opera/sds/pcm/testdata_R2.0.0/deployment_smoke_test_rtc_s1_${rtc_v}.zip
+unzip deployment_smoke_test_rtc_s1_${rtc_v}.zip
+unzip deployment_smoke_test_cslc_s1_${cslc_v}.zip
 
 echo "Sleeping 1 hr initially for the SCIFLO PGE jobs to complete..."
 sleep 3600
@@ -50,7 +50,7 @@ cd ..
 
 # Run comparisons
 echo "Comparing RTC products against gold files"
-./rtc_compare.sh ./output_dir_rtc ./deployment_smoke_test_rtc_s1_calval_${rtc_v} | grep "file: OPERA_L2_RTC-S1_T064"
+./rtc_compare.sh ./output_dir_rtc ./deployment_smoke_test_rtc_s1_${rtc_v} | grep "file: OPERA_L2_RTC-S1_T064"
 
 echo "Comparing CSLC products against gold files"
-./cslc_compare.sh ./output_dir_cslc ./deployment_smoke_test_cslc_s1_calval_${cslc_v}
+./cslc_compare.sh ./output_dir_cslc ./deployment_smoke_test_cslc_s1_${cslc_v}
