5e4e501fb608278a2098feb5ee43b762faada58c
philipjyoon <104090313+philipjyoon@users.noreply.github.com>
2024-01-05 13:56:01 -0800

    Update delete_grq_es_data_indices.py
    Updates for R3 catalogs

--- delete_grq_es_data_indices.py
+++ delete_grq_es_data_indices.py
@@ -5,6 +5,6 @@ grq_es = get_grq_es()
 print("Deleting data indices from", grq_es.es_url)
 
 for index in grq_es.es.indices.get_alias('*').keys():
-    if index[:4] == 'grq_' or index[:4] == 'hls_' or index[:4] == 'slc_' or index[:5] == 'jobs_':
+    if index[:4] == 'grq_' or index[:4] == 'hls_' or index[:4] == 'slc_' or index[:5] == 'jobs_' or index[:5] == 'cslc_' or index[:4] == 'rtc_':
         print("Deleting index", index)
         print(grq_es.es.indices.delete(index=index, ignore=[404]))
