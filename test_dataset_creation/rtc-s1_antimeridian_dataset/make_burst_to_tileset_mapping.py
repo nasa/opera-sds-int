9524220004c079f558328bc2a069e43bf5a78924
Steven Lewis <Steven.J.Lewis@jpl.nasa.gov>
2024-03-12 12:42:59 -0700

    Fixed a bug in the for loop, and added the ability for the script to return bursts over land tiles only, or water tiles only, or both

--- test_dataset_creation/rtc-s1_antimeridian_dataset/make_burst_to_tileset_mapping.py
+++ test_dataset_creation/rtc-s1_antimeridian_dataset/make_burst_to_tileset_mapping.py
@@ -4,13 +4,21 @@ import sqlite3
 import numpy as np
 import pandas as pd
 
+'''
+
+'''
+
+class LAND_FLAG:
+    LAND_ONLY = 100
+    WATER_ONLY = 200
+    BOTH = 300
 
 
 def flatten(xss):
     return [x for xs in xss for x in xs]
 
 
-def get_mgrs_burst_db_from_sqlite_file(mgrs_tileset_db_filepath):
+def get_mgrs_burst_db_from_sqlite_file(mgrs_tileset_db_filepath, land_flag=LAND_FLAG.BOTH):
 
     cnx = sqlite3.connect(mgrs_tileset_db_filepath)
 
@@ -18,7 +26,16 @@ def get_mgrs_burst_db_from_sqlite_file(mgrs_tileset_db_filepath):
     #df.columns
 
     cnx.close()
-    
+
+    if land_flag == LAND_FLAG.LAND_ONLY:
+        df = df[df['land_ocean_flag']!='water']
+    elif land_flag == LAND_FLAG.WATER_ONLY:
+        df = df[df['land_ocean_flag']=='water']
+    elif land_flag == LAND_FLAG.BOTH:
+        pass
+    else:
+        error('bad value of land_flag')
+        
     return df
 
 
@@ -41,9 +58,9 @@ def get_bursts_from_query(rtc_burst_list_filename):
     return bursts_from_query
 
 
-def map_bursts_to_tilesets(rtc_burst_list_filename):
+def map_bursts_to_tilesets(rtc_burst_list_filename, land_flag):
 
-    df = get_mgrs_burst_db_from_sqlite_file('/Users/Sjlewis/proj/src-git/opera/dswx_s1_burst_db_data/MGRS_tile_collection_v0.3.sqlite')
+    df = get_mgrs_burst_db_from_sqlite_file('/Users/Sjlewis/proj/src-git/opera/dswx_s1_burst_db_data/MGRS_tile_collection_v0.3.sqlite', land_flag)
 
     #burst_array = np.array(df.bursts)
 
@@ -68,14 +85,19 @@ def map_bursts_to_tilesets(rtc_burst_list_filename):
     # This will take a while
     # magic number:  3 is the column to get the burstID from the granule ID
     tilesets = [list(np.where(df['bursts'].str.contains(burst.split('_')[3].lower().replace('-','_')))[0]) for burst in bursts_from_query]
-
+    
     tilesets_unique = np.unique(flatten(tilesets))
 
-    print(df['mgrs_set_id'][tilesets_unique])
+    #print(df['mgrs_set_id'][tilesets_unique])
     # Here's what will get printed out for the antimeridian case
     #['MS_1_58','MS_1_59','MS_1_60','MS_1_61','MS_30_65','MS_30_66','MS_66_40','MS_66_41','MS_66_171','MS_66_172','MS_81_29','MS_81_30','MS_146_103','MS_146_104','MS_146_105']
+    # New ones:  'MS_66_39', 'MS_66_170', 
 
-
+    #tilesets_unique_land = tilesets_unique[df['land_ocean_flag'][tilesets_unique] != 'water']
+    #print(df['mgrs_set_id'][tilesets_unique_land])
+    # Here's what will get printed out for the antimeridian case (using data through 2024-03-11):
+    # MS_1_58, MS_66_39, MS_66_40, MS_66_171, MS_66_172, MS_146_103, MS_146,104
+    
     indices_with_unique_tiles = np.array([len(a)==1 for a in tilesets])
 
     bursts_antimeridian_with_unique_tiles = bursts_from_query[indices_with_unique_tiles]
@@ -84,22 +106,23 @@ def map_bursts_to_tilesets(rtc_burst_list_filename):
     # I just want a numpy array of the indices of the tilesets in the df
     tilesets_for_bursts_with_unique_tiles = np.array(flatten(list(np.array(tilesets, dtype=object)[indices_with_unique_tiles])))
 
-    bursts_antimeridian_with_unique_tiles[tilesets_for_bursts_with_unique_tiles == tilesets_unique[0]]
+    ##bursts_antimeridian_with_unique_tiles[tilesets_for_bursts_with_unique_tiles == tilesets_unique[0]]
 
     # get the date of the first entry of the first tileset
-    bursts_antimeridian_with_unique_tiles[tilesets_for_bursts_with_unique_tiles == tilesets_unique[0]][0].split('_')[4].split('T')[0]
+    ##bursts_antimeridian_with_unique_tiles[tilesets_for_bursts_with_unique_tiles == tilesets_unique[0]][0].split('_')[4].split('T')[0]
 
     # unique dates for the first tileset
-    np.unique([b.split('_')[4].split('T')[0] for b in bursts_antimeridian_with_unique_tiles[tilesets_for_bursts_with_unique_tiles == tilesets_unique[0]]])
+    ##np.unique([b.split('_')[4].split('T')[0] for b in bursts_antimeridian_with_unique_tiles[tilesets_for_bursts_with_unique_tiles == tilesets_unique[0]]])
 
-    dates, inds = np.unique([b.split('_')[4].split('T')[0] for b in bursts_antimeridian_with_unique_tiles[tilesets_for_bursts_with_unique_tiles == tilesets_unique[0]]], return_index=True)
+    #dates, inds = np.unique([b.split('_')[4].split('T')[0] for b in bursts_antimeridian_with_unique_tiles[tilesets_for_bursts_with_unique_tiles == tilesets_unique[0]]], return_index=True)
 
-    print(bursts_antimeridian_with_unique_tiles[tilesets_for_bursts_with_unique_tiles == tilesets_unique[0]][inds])
+    #print(bursts_antimeridian_with_unique_tiles[tilesets_for_bursts_with_unique_tiles == tilesets_unique[0]][inds])
 
 
     for t in np.unique(tilesets_for_bursts_with_unique_tiles):
-        np.unique([b.split('_')[4].split('T')[0] for b in bursts_antimeridian_with_unique_tiles[tilesets_for_bursts_with_unique_tiles == t]])
-        dates, inds = np.unique([b.split('_')[4].split('T')[0] for b in bursts_antimeridian_with_unique_tiles[tilesets_for_bursts_with_unique_tiles == tilesets_unique[0]]], return_index=True)
+        #np.unique([b.split('_')[4].split('T')[0] for b in bursts_antimeridian_with_unique_tiles[tilesets_for_bursts_with_unique_tiles == t]])
+        dates, inds = np.unique([b.split('_')[4].split('T')[0] for b in bursts_antimeridian_with_unique_tiles[tilesets_for_bursts_with_unique_tiles == t]], return_index=True)
+        #dates, inds = np.unique([b.split('_')[4].split('T')[0] for b in bursts_antimeridian_with_unique_tiles[tilesets_for_bursts_with_unique_tiles == tilesets_unique[0]]], return_index=True)
         print(bursts_antimeridian_with_unique_tiles[tilesets_for_bursts_with_unique_tiles == t][inds])
 
         pass
@@ -109,7 +132,16 @@ def map_bursts_to_tilesets(rtc_burst_list_filename):
 
 
 if __name__ == '__main__':
-    #map_bursts_to_tilesets(rtc_burst_list_filename = 'rtc-s1_antimeridian_through_2024-02-08_4638260145-download-s3.txt')
+    map_bursts_to_tilesets(rtc_burst_list_filename = ['rtc-s1_antimeridian_2023-10-04_thru_2024-02-08_4638260145-download-s3.txt',
+                                                      'rtc-s1_antimeridian_2024-02-09_thru_2024-03-11_6031864814-download-s3.txt',
+                                                      ],
+                           land_flag = LAND_FLAG.LAND_ONLY,
+    )
 
-    map_bursts_to_tilesets(rtc_burst_list_filename = ['rtc-s1_high-latitude_2023_4539263142-download-s3.txt', 'rtc-s1_high-latitude_2024_6303412449-download-s3.txt'])
+    map_bursts_to_tilesets(rtc_burst_list_filename = ['rtc-s1_high-latitude_2023-10-04_thru_2023-12-31_4539263142-download-s3.txt',
+                                                      'rtc-s1_high-latitude_2024-01-01_thru_2024-02-11_6303412449-download-s3.txt',
+                                                      'rtc-s1_high-latitude_2024-02-12_thru_2024-03-11_4630866141-download-s3.txt',
+                                                      ],
+                           land_flag = LAND_FLAG.LAND_ONLY,
+    )
 
