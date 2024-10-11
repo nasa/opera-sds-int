4a123722a37fd8fe8ca9941ecd7c7b867dbd931c
Philip Yoon <philip.j.yoon@jpl.nasa.gov>
2024-10-10 20:40:21 -0700

    Adding k_cslc* to GRQ ES to delete when reseting cluster

--- delete_grq_es_data_indices.py
+++ delete_grq_es_data_indices.py
@@ -5,6 +5,7 @@ grq_es = get_grq_es()
 print("Deleting data indices from", grq_es.es_url)
 
 for index in grq_es.es.indices.get_alias('*').keys():
-    if index[:4] == 'grq_' or index[:4] == 'hls_' or index[:4] == 'slc_' or index[:5] == 'jobs_' or index[:5] == 'cslc_' or index[:4] == 'rtc_' or index == 'batch_proc':
+    if (index[:4] == 'grq_' or index[:4] == 'hls_' or index[:4] == 'slc_' or index[:5] == 'jobs_' or
+            index[:5] == 'cslc_' or index[:7] == 'k_cslc_' or index[:4] == 'rtc_' or index == 'batch_proc'):
         print("Deleting index", index)
         print(grq_es.es.indices.delete(index=index, ignore=[404]))
